adecd532d7ed880fa34ba0aa36b7c560711c32b4
Alex Gramm <82889516+Agramm18@users.noreply.github.com>
2024-04-17 18:44:05 +0200

    Add files via upload

---
+++ Amarican City Namer.py
@@ -0,0 +1,13 @@
+
+
+while True:
+
+    City_Name = input("Name a European City: ")
+
+    if City_Name == "Ende":
+        break
+
+    Result = City_Name
+
+    American_City_Name = print(f"New {Result}")
+    
